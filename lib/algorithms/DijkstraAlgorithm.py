23e8ca82a1a93fabbfb548f0d7d381f879d858ad
Remi Gascou <remi@gascou.net>
2018-11-13 07:45:23 +0100

    Dikstra

--- lib/algorithms/DijkstraAlgorithm.py
+++ lib/algorithms/DijkstraAlgorithm.py
@@ -8,31 +8,43 @@ class DijkstraAlgorithm(object):
         super(DijkstraAlgorithm, self).__init__()
         self.graph = graph
         self.table = {}
+        self.choices = {}
 
     def run(self, labelNodeStart, labelNodeDest):
         if self.isGraphPositive():
             #Init
-            self.table[labelNodeStart] = []
+            self.choices[labelNodeStart] = [0]
             for node in self.graph.get_nodes():
                 if node.get_label() != labelNodeStart:
                     self.table[str(node.get_label())] = []
-            print(self.table)
             #Processing
             labelselected = labelNodeStart
-            del self.table[labelselected]
             while labelselected != labelNodeDest:
+                print("labelselected : ", labelselected)
                 ## Update table :
                 for edge in self.graph.get_edges():
                     if edge.get_nodeStart().get_label() == labelselected:
-                        self.table[str(edge.get_nodeDest().get_label())].append(edge.get_capacity())
+                        if len(self.table[str(edge.get_nodeDest().get_label())]) == 0: self.table[str(edge.get_nodeDest().get_label())].append(edge.get_capacity())
+                        else: self.table[str(edge.get_nodeDest().get_label())].append(self.table[str(edge.get_nodeDest().get_label())][-1] + edge.get_capacity())
                 ## Seek minimum
-                # mini, labelmini = -1, ""
-                # for e in self.table:
-                #     if mini == -1 and len(self.table[e]) != 0: mini, labelmini = self.table[e][-1], e
-                #     elif self.table[e][-1] < mini: mini, labelmini = self.table[e][-1], e
-                #
-                # labelselected = labelmini
-                print(self.table)
+                mini, labelmini = -1, ""
+                for e in self.table:
+                    if len(self.table[e]) != 0:
+                        if mini == -1:
+                            mini, labelmini = self.table[e][-1], e
+                        elif self.table[e][-1] <= mini:
+                            mini, labelmini = self.table[e][-1], e
+
+                print("Old labelselected :", labelselected)
+                if labelselected != labelNodeStart:
+                    self.choices[str(labelselected)] = self.table[str(labelselected)][-1]
+                labelselected = labelmini
+                print("New labelselected :", labelselected)
+                print("labelmini :", labelmini)
+                print("self.table :", self.table)
+                print("self.choices :", self.choices)
+                print("\n")
+
             #Return
             if len(self.table[labelNodeStart]) == 0: return None
             else : return self.table[labelNodeStart][-1]
--- lib/testcases/DijkstraAlgorithm_testcase.py
+++ lib/testcases/DijkstraAlgorithm_testcase.py
@@ -8,7 +8,7 @@ def test(g:Graph, AlgorithmClass, args, expectedresult):
     except AssertionError: print("FailedTest : (result=" + str(result) + ", expectedresult=" + str(expectedresult) + ")")
 
 
-g1 = Graph(["S", "A", "B", "C", "D", "T"], [["S", "A", 10], ["S", "C", 10], ["A", "B", 4], ["A", "D", 8], ["A", "C", 2], ["C", "D", 9], ["D", "B", 6], ["B", "T", 10], ["D", "T", 10]])
+g1 = Graph([c for c in "ABCDEFGH"], [["A", "B", 12], ["A", "D", 14], ["B", "G", 16], ["B", "H", 21], ["B", "F", 9], ["G", "H", 11], ["F", "H", 11], ["E", "C", 13], ["E", "F", 16], ["E", "H", 10], ["C", "F", 10]])
 
-args=["A", "B"]
-test(g1, DijkstraAlgorithm, args, 19)
+args=["A", "H"]
+test(g1, DijkstraAlgorithm, args, 32)
